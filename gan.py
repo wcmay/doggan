9669179906abb02bac5e9fe01e69f87266fbb253
Carl <carlssonmay@gmail.com>
2023-11-28 20:44:59 -0500

    Tweaks/Bug Fixes
    Slightly changed the design of the neural nets and fixed a (conceptual) bug

--- gan.py
+++ gan.py
@@ -3,20 +3,19 @@ import numpy as np
 import torch.nn as nn
 import torch.optim as optim
 
-# Last Modified: CM 11/27
+# Last Modified: CM 11/28
 class Generator(nn.Module):
 
 	# Layer sizes is a list of ints corresponding to the size of each layer
 	# Eg: [6, 4, 1]
-    def __init__(self, input_size, layer_sizes, output_size, drop_prob = 0.5):
+    def __init__(self, input_size, layer_sizes, drop_prob = 0.0):
         super(Generator, self).__init__()
 
         self.num_layers = len(layer_sizes)
         self.layers = []
         self.layers.append(nn.Linear(input_size, layer_sizes[0]))
         for i in range(self.num_layers - 1):
-        	self.layers.append(nn.Linear(layer_sizes[i+1], layer_sizes[i+2]))
-        self.layers.append(nn.Linear(layer_sizes[-1], output_size))
+        	self.layers.append(nn.Linear(layer_sizes[i], layer_sizes[i+1]))
 
         self.dropout = nn.Dropout(drop_prob)
         self.act = nn.ReLU()
@@ -25,28 +24,29 @@ class Generator(nn.Module):
     # Pytorch automatically handles backward for us once we have defined forward
     def forward(self, x):
         for i in range(self.num_layers - 1):
-        	x = layers[i](x)
+            l_0 = layers[i]
+        	x = l_0(x)
         	x = self.act(x)
         	x = self.dropout(x)
-        x = layers[-1](x)
+        l_0 = layers[-1]
+        x = l_0(x)
         x = self.sigmoid(x)
         return x
 
-# Last Modified: CM 11/27
+# Last Modified: CM 11/28
 class Discriminator(nn.Module):
 
 	# Layer sizes is a list of ints corresponding to the size of each layer
 	# Eg: [6, 4, 1]
 	# Discriminator output size should always be 1 (bc it's just true or false)
-    def __init__(self, input_size, layer_sizes, drop_prob = 0.5):
+    def __init__(self, input_size, layer_sizes, drop_prob = 0.0):
         super(Discriminator, self).__init__()
 
         self.num_layers = len(layer_sizes)
         self.layers = []
         self.layers.append(nn.Linear(input_size, layer_sizes[0]))
         for i in range(self.num_layers - 1):
-        	self.layers.append(nn.Linear(layer_sizes[i+1], layer_sizes[i+2]))
-        self.layers.append(nn.Linear(layer_sizes[-1], 1))
+        	self.layers.append(nn.Linear(layer_sizes[i], layer_sizes[i+1]))
 
         self.dropout = nn.Dropout(drop_prob)
         self.act = nn.ReLU()
@@ -55,10 +55,12 @@ class Discriminator(nn.Module):
     # Pytorch automatically handles backward for us once we have defined forward
     def forward(self, x):
         for i in range(self.num_layers - 1):
-        	x = layers[i](x)
-        	x = self.act(x)
-        	x = self.dropout(x)
-        x = layers[-1](x)
+            l_0 = layers[i]
+            x = l_0(x)
+            x = self.act(x)
+            x = self.dropout(x)
+        l_0 = layers[-1]
+        x = l_0(x)
         x = self.sigmoid(x)
         return x
 
@@ -71,7 +73,7 @@ def train(G, D):
     g_optimizer = optim.SGD(G.parameters(), lr=g_learning_rate)
 
 def main():
-	G = Generator(1, [1], 1)
+	G = Generator(1, [1])
 	D = Discriminator(1, [1])
     train(G, D)
 
