64218bb1620ab0bd608106e35e313dadc734ee12
Carl <carlssonmay@gmail.com>
2023-12-01 00:41:45 -0500

    600 CATS

--- gan.py
+++ gan.py
@@ -21,7 +21,7 @@ class GANNet(nn.Module):
         self.num_layers = 0
         for i in range(len(layer_sizes) - 1):
             self.layers.append(nn.Linear(layer_sizes[i], layer_sizes[i+1], bias=False)) # TODO: wtf is bias?
-            nn.init.normal_(self.layers[i].weight) # Do we need this? (May be initializing weights)
+            #nn.init.normal_(self.layers[i].weight) # Do we need this? (May be initializing weights)
             self.num_layers += 1
 
         self.dropout = nn.Dropout(drop_prob)
@@ -45,7 +45,7 @@ class GANNet(nn.Module):
 # TODO: Write a function that tests the Generator and Discriminator classes
 # Last Modified: CM 11/27
 def train(G, D, training_images):
-    D_learning_rate = 0.01
+    D_learning_rate = 0.05
     G_learning_rate = 0.01
     max_epochs = 100
     loss = nn.MSELoss()
@@ -98,16 +98,19 @@ def train(G, D, training_images):
             # print(str(epoch) + " D Fake Predict: " + str(fake_data_D_out))
             # print(str(epoch) + " D Real Predict: " + str(true_data_D_out))
 
-            D_mean_losses.append(D_loss.item())
-            G_mean_losses.append(G_loss.item())
+            D_mean_losses.append(D_loss.detach().item())
+            G_mean_losses.append(G_loss.detach().item())
 
             if i == training_set_size - 1:
                 torch_fake_images.append(fake_data.detach())
 
-        export_image(torch_fake_images[epoch].numpy(), 'gen_' + str(epoch))
+        export_image(torch_fake_images[-1].numpy(), 'gen_' + str(epoch))
         export_image(torch_training_images[indices[0]].numpy(), 'true_' + str(epoch))
 
-        print("Epoch " + str(epoch) + ": D Loss " + str(torch.mean(D_loss.detach()).item()) + ", G Loss " + str(torch.mean(G_loss.detach()).item()))
+        print("Epoch " + str(epoch) + ": D Loss " + str(D_mean_losses[-1]) + ", G Loss " + str(G_mean_losses[-1]))
+
+        #for layer in G.layers:
+        #    print(layer.weight)
 
 
 # Preconditions: 
@@ -120,14 +123,14 @@ def export_image(i, filename):
     image = i * 255
     image = np.reshape(image, (128, 128))
     image = image.astype(np.uint8)
-    ski.io.imsave(getcwd() + "/exported/" + filename + ".jpg", ski.color.gray2rgb(image))
+    ski.io.imsave(getcwd() + "/exported/" + filename + ".jpg", ski.color.gray2rgb(image), check_contrast=False)
 
 
 def main():
     # CHANGE THESE VARIABLES
     # Possible choices: "dog", "cat", "corgi"
     animal_type = "corgi"
-    max_training_set_size = 200
+    max_training_set_size = 100
 
     list_files = listdir(getcwd() + "/afhq/" + animal_type)
 
